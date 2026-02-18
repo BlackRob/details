e8ce270e3c5eb381742c7900f759012e68f9dc56
Robert Austin <robert.w.austin@gmail.com>
2026-02-18 15:30:09 -0500

    Configure webpack to externalize sharp for Netlify

--- next.config.js
+++ next.config.js
@@ -1,21 +1,12 @@
 module.exports = {
-  webpack: (cfg) => {
-    // 1. KEEP your existing rules for Markdown and Fonts
-    cfg.module.rules.push(
-      {
-        test: /\.md$/,
-        loader: "frontmatter-markdown-loader",
-        options: { mode: ["react-component"] },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
-      },
-    );
-
-    // 2. ADD this line to fix the Next.js 12 hanging bug
-    cfg.cache = false;
-
+  webpack: (cfg, { isServer }) => {
+    if (isServer) {
+      cfg.externals = cfg.externals || [];
+      cfg.externals.push({
+        sharp: "commonjs sharp",
+      });
+    }
+    
     return cfg;
   },
 };
